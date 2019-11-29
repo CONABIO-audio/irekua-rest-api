264a8dcb86fe5f10604037facef71f3857f2b237
mbsantiago <santiago.mbal@gmail.com>
2019-11-29 00:09:23 -0600

    fixed base dir in settings file

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,14 @@
+import os
+
 from irekua_dev_settings.settings import *
 from irekua_database.settings import *
 from irekua_rest_framework.settings import *
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
+
 
 INSTALLED_APPS = (
     IREKUA_REST_API_APPS +
